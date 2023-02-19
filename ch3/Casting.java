a1c7fcf9eca6fab7aa4370163808e9ba8b93ab4b
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-02-19 18:03:08 +0000

    Add files via upload

---
+++ ch3/Casting.java
@@ -0,0 +1,44 @@
+package ch3;
+
+public class Casting {
+    public static void main(String[] args) {
+/*
+        System.out.println("Float: " + Float.MAX_VALUE);// Float: 3.4028235E38
+        System.out.println("Float: " + Float.MIN_VALUE);// Float: 1.4E-45
+        System.out.println("Long: " + Long.MAX_VALUE);  // Long: 9223372036854775807
+        System.out.println("Long: " + Long.MIN_VALUE);  // Long: -9223372036854775808
+*/
+        // Widening
+//        final byte b = 5;
+//        char c = b;
+/*
+        char c   = 'a'; // normal
+        int i    = c;   // widening, char to int
+        float f  = i;   // widening, int to float
+        double d = f;   // widening, float to double
+        float f2 = 1L;  // widening, long to float
+*/
+
+        // Narrowing
+      /*  int i   = (int)3.3;                         // narrowing, double to int
+        byte b  = (byte) 233;                       // narrowing, int to byte
+        float f = (float) 3.5;                      // narrowing, double to float
+        System.out.println(i + " " + b + " " +f);   // 3 -23 3.5*/
+
+        // Special Rules for byte,char,short and int
+
+//        byte b  = 127;          // narrowing, int to byte, special rule integral
+//        byte b2 = 128;          // out of range error
+        char c = 12;
+        char c2 = 90_000;
+        short s = 12;           // 12 is a literal, ok
+        s = c;
+        s = (short) c;
+        c = s;
+        c = (char) s;
+
+        final char c1 = 12;
+        short s1 = 12;           // 12 is a literal, ok
+        s1 = c1;
+    }
+}
