2b4c2e4cb3dffda6e4512214c217d2bef202a059
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-02-19 18:05:17 +0000

    Add files via upload

---
+++ ch2/VariablesAndPrimitiveDataTypes.java
@@ -0,0 +1,62 @@
+package ch2;
+
+public class VariablesAndPrimitiveDataTypes {
+    public static void main(String[] args) {
+//        int age = 25;
+//        System.out.println(age);
+
+        int ageOfCar;
+        int numberOfChildren;
+        //char c = 9;
+
+        // valid identifiers
+        int a£€_23, _29, Z, thisIsAnExampleOfAVeryLongVariableName, €2_, $4;
+
+        // invalid identifiers
+       /* int 9age;
+        int abc def;
+        int d
+               j;*/
+
+        /*short s1 = 5_000;
+        short s2 = 50_000;*/
+
+        // decimal
+        int dec = 30;
+        System.out.println(dec);
+
+        // hexadecimal = 16 + 14
+        int hex = 0x1E;
+        System.out.println(hex);
+
+        // binary = 16 + 8 + 4 + 2
+        int bin = 0b11110;
+        System.out.println(bin);
+
+        int x  = 10;
+        long n = 10L;
+
+        double d = 10.34;
+        float f  = 10.34F;
+
+        char c = 'a';
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+    }
+}
+/*
+        // decimal
+        int dec = 30;
+        System.out.println(dec);
+
+        // hex = 16 + 14
+        int hex = 0x1E;
+        System.out.println(hex);
+
+        // binary = 16 + 8 + 4 + 2
+        int bin = 0b11110;
+        System.out.println(bin);
+
+ */
