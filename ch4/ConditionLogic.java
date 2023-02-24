791756b862f373b2a850d0d3cdad5464e1b0ddce
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-02-24 16:35:42 +0000

    Add files via upload

---
+++ ch4/ConditionLogic.java
@@ -0,0 +1,10 @@
+package ch4;
+
+public class ConditionLogic {
+    public static void main(String[] args) {
+        ifStmt();
+    }
+    public static void ifStmt(){
+
+    }
+}
--- ch4/Scope.java
+++ ch4/Scope.java
@@ -2,8 +2,15 @@ package ch4;
 
 public class Scope {
     public static void main(String[] args) {
-        int y = 4;
-        y++;
+        int x = 1;
+        x++;
+
+        { // start of block
+            int y = 2;
+            y++;
+            x++;
+        } // end of block
+        x++;
+        y++; // out of scope
     }
-    y++; // out of scope
 }
