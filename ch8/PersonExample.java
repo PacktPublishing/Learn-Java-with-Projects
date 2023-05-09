9c997593eef56f3be826d3336c6005d464fb954e
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-05-09 11:09:50 +0100

    Add files via upload

--- ch8/PersonExample.java
+++ ch8/PersonExample.java
@@ -1,6 +1,39 @@
 package ch8;
 
 class Person{
+    private String name;    // instance variable
+
+    public Person(String aName) { // constructor
+//        name = aName;
+        this.name = aName;
+    }
+    public String getName() { // instance method
+//        return name;
+        return this.name;
+    }
+    public void setName(String aName) { // instance method
+//        name = aName;
+        this.name = aName;
+    }
+}
+public class PersonExample {
+    public static void main(String[] args) {
+        Person p1 = new Person("Maaike");
+        Person p2 = new Person("Sean");
+        System.out.println(p1.getName()); // Maaike
+        System.out.println(p2.getName()); // Sean
+        p1.setName("Maaike van Putten");
+        p2.setName("Sean Kennedy");
+        System.out.println(p1.getName()); // Maaike van Putten
+        System.out.println(p2.getName()); // Sean Kennedy
+    }
+}
+// 1. refactor - DONE
+// 2. this.name = name
+// 3. accessing instance from static method
+// 4. reference versus objects - highlight! - DONE
+/*
+class Person{
     private String name;        // instance variable
     private static int count;   // class variable
 
@@ -25,6 +58,8 @@ public class PersonExample {
         System.out.println(Person.getCount()); // 2
     }
 }
+
+ */
 /*
 class Person{
     private String name;    // instance variable
