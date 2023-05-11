0b6629840aa30b40ca1e59ba38dabab6adfa9298
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-05-11 14:57:25 +0100

    Add files via upload

--- ch8/PersonExample.java
+++ ch8/PersonExample.java
@@ -1,5 +1,68 @@
 package ch8;
 
+public class PersonExample {
+    int x;              // instance variable
+    public void m(){}   // instance method
+    public static void main(String[] args) {
+        // Non-static field 'x' cannot be referenced
+        // from a static context
+//        x = 9;  // same as 'this.x = 9;'
+//        this.x = 99;
+//        m();    // same as 'this.m();'
+//        this.m();
+
+        // this works
+        PersonExample pe = new PersonExample();
+        pe.x=999;   // ok
+        pe.m();     // ok
+        System.out.println(pe.x);   // 999
+    }
+}
+/*
+class Person{
+    private String name;    // instance variable
+
+    public Person(String name) { // constructor
+        this.name = name;
+    }
+    public String getName() { // instance method
+        return name;
+    }
+    public void setName(String aName) { // instance method
+        name = aName;
+    }
+}
+public class PersonExample {
+    public static void main(String[] args) {
+        Person p1 = new Person("Maaike");
+        System.out.println(p1.getName()); // Maaike
+    }
+}
+
+ */
+/*
+class Person{
+    private String name;    // instance variable
+
+    public Person(String name) { // constructor
+        name = name; // shadowing/hiding the instance variable
+    }
+    public String getName() { // instance method
+        return name;
+    }
+    public void setName(String aName) { // instance method
+        name = aName;
+    }
+}
+public class PersonExample {
+    public static void main(String[] args) {
+        Person p1 = new Person("Maaike");
+        System.out.println(p1.getName()); // null
+    }
+}
+
+ */
+/*
 class Person{
     private String name;    // instance variable
 
@@ -28,10 +91,8 @@ public class PersonExample {
         System.out.println(p2.getName()); // Sean Kennedy
     }
 }
-// 1. refactor - DONE
-// 2. this.name = name
-// 3. accessing instance from static method
-// 4. reference versus objects - highlight! - DONE
+
+ */
 /*
 class Person{
     private String name;        // instance variable
