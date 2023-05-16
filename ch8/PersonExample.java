9656aff71bf79d440e0973fbd78373c6a7e937dd
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-05-16 16:41:47 +0100

    Add files via upload

--- ch8/PersonExample.java
+++ ch8/PersonExample.java
@@ -2,11 +2,9 @@ package ch8;
 
 class Person{
     private String name;    // instance variable
-    private int count;      // instance variable
 
-    Person(String aName) { // constructor
-        name = aName;
-        count++;
+    Person(String name) { // constructor
+        this.name = name;
     }
     public String getName() { // instance method
         return name;
@@ -14,20 +12,11 @@ class Person{
     public void setName(String aName) { // instance method
         name = aName;
     }
-    public int getCount() { // instance method
-        return count;
-    }
 }
 public class PersonExample {
     public static void main(String[] args) {
         Person p1 = new Person("Maaike");
-        Person p2 = new Person("Sean");
         System.out.println(p1.getName()); // Maaike
-        System.out.println(p2.getName()); // Sean
-        p1.setName("Maaike van Putten");
-        p2.setName("Sean Kennedy");
-        System.out.println(p1.getName()); // Maaike van Putten
-        System.out.println(p2.getName()); // Sean Kennedy
     }
 }
 
