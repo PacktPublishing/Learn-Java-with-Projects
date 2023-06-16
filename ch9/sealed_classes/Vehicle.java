fce08c7fde9f6145775f8b714d6e1dd98f7e3bd3
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-06-16 13:48:34 +0100

    Add files via upload

---
+++ ch9/sealed_classes/Vehicle.java
@@ -0,0 +1,25 @@
+package ch9.sealed_classes;
+
+public sealed class Vehicle permits Car{ }  // scoping hierarchy
+sealed class Car extends Vehicle permits Saloon {}
+sealed class Truck extends Vehicle {}       // compiler error
+non-sealed class Saloon extends Car{}       // opening up hierarchy again
+class Volvo extends Saloon{}
+class Convertible extends Car{}             // compiler error
+
+
+//class Table implements Driveable{}
+
+
+/*
+sealed interface Driveable permits Vehicle{
+}
+public sealed class Vehicle implements Driveable permits Car{
+}
+sealed class Car extends Vehicle permits Ford, Volvo{}
+
+final class Ford extends Car{}
+final class Volvo extends Car{}
+
+//class Table implements Driveable{}
+ */
