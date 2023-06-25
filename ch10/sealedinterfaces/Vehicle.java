be8199f6a8e3746aacef3b0708b8de31775c00ea
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-06-25 14:49:54 +0100

    Add files via upload

---
+++ ch10/sealedinterfaces/Vehicle.java
@@ -0,0 +1,16 @@
+package ch10.sealedinterfaces;
+
+sealed interface Driveable permits Vehicle{ } // scoping interface hierarchy
+public sealed class Vehicle implements Driveable permits Car{ }
+
+sealed class Car extends Vehicle permits Saloon {}
+non-sealed class Saloon extends Car{}       // opening up hierarchy again
+class Volvo extends Saloon{}
+class Ford extends Saloon{}
+
+class Window extends Vehicle{}      // Vehicle permits Car only
+class Chair extends Car{}           // Car permits Saloon only
+class Table implements Driveable{}  // Driveable permits Vehicle only
+
+//class Monkey extends Saloon{}
+
