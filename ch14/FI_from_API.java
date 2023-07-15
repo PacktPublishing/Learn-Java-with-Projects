5fa98fbf453df190cac42abaaa0f3cc22402be32
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-07-15 09:08:03 +0100

    Add files via upload

--- ch14/FI_from_API.java
+++ ch14/FI_from_API.java
@@ -1,32 +1,23 @@
 package ch14;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
-
-import java.util.function.UnaryOperator; // add by 1 is a unary operation
-import java.util.function.BinaryOperator;// multiplying 2 numbers is a binary operation
+import java.util.function.*;
 
 public class FI_from_API {
 
     public static void main(String[] args) {
         FI_from_API fiAPI = new FI_from_API();
 //        fiAPI.predicate();
-        fiAPI.supplier();
+//        fiAPI.supplier();
 //        fiAPI.consumer();
 //        fiAPI.function();
-//        fiAPI.unaryBinaryOperator();
+        fiAPI.unaryBinaryOperator();
     }
     public void predicate() {
         // Predicate<T> is a functional interface i.e. one abstract method:
@@ -39,19 +30,36 @@ public class FI_from_API {
         BiPredicate<String, Integer> checkLength = (str, len) -> str.length() == len;
         System.out.println(checkLength.test("Vatican City", 8));//false (length is 12)
 
+        DoublePredicate dblPred = primDbl -> Math.pow(primDbl, 2) > 100;
+        System.out.println(dblPred.test(5));    // false
+        System.out.println(dblPred.test(25));   // true
+
+        IntPredicate intPred = primInt -> Math.pow(primInt, 2) > 100;
     }
 
     public void supplier() {
         // Supplier<T> is a functional interface i.e. one abstract method:
         //      T get()
         Supplier<StringBuilder> supSB = () -> new StringBuilder();
-        System.out.println("Supplier SB: " + supSB.get().append("SK"));// Supplier SB: SK
+        System.out.println(supSB.get().append("SK"));// SK
         
         Supplier<LocalTime> supTime = () -> LocalTime.now();
-        System.out.println("Supplier time: " + supTime.get());//Supplier time: 09:11:39.121101600
+        System.out.println(supTime.get());  // 11:00:50.769271700
         
         Supplier<Double> sRandom = () -> Math.random();
-        System.out.println(sRandom.get());// e.g. 0.782467864130131
+        System.out.println(sRandom.get());  // 0.13482391499981883
+
+        BooleanSupplier testLeapYear = () -> LocalDate.now().isLeapYear();
+        System.out.println(testLeapYear.getAsBoolean()); // false (2023)
+
+        DoubleSupplier r = () -> Math.random();
+        System.out.println(r.getAsDouble());  // 0.13719533755736413
+
+        IntSupplier iS = () -> (int)(Math.random()*20);
+        System.out.println(iS.getAsInt());  // 17
+
+        LongSupplier lgS = () -> (long)(Math.random()*100);
+        System.out.println(lgS.getAsLong());  // 50
     }
 
     public void consumer() {
@@ -61,47 +69,70 @@ public class FI_from_API {
         printC.accept("To be or not to be, that is the question");
         
         List<String> names = new ArrayList<>();
-        names.add("John");names.add("Mary");
-        names.forEach(printC);  // John, Mary
+        names.add("Maaike");names.add("Sean");
+        names.forEach(printC);  // Maaike, Sean
 
+        Map<String,String> mapCapitalCities = new HashMap<>();
         // BiConsumer<T, U> is a functional interface i.e. one abstract method:
         //      void accept(T t, U u)
-        Map mapCapitalCities = new HashMap<String, String>();
-        // Note: The return value of put(k,v) is just ignored (and not returned from the lambda)
-        BiConsumer<String, String> biCon = (key, value) -> mapCapitalCities.put(key, value);
+        // Note: Object put(k,v) - return value ignored.
+        //       This Consumer has a side-effect.
+        BiConsumer<String, String> biCon = (key, value) ->
+                                            mapCapitalCities.put(key, value);
         biCon.accept("Dublin", "Ireland");
-        biCon.accept("Washington D.C.", "USA");
-        System.out.println(mapCapitalCities);// {Dublin=Ireland, Washington D.C.=USA}
+        biCon.accept("The Hague", "Holland");
+        System.out.println(mapCapitalCities);// {Dublin=Ireland, The Hague=Holland}
 
         BiConsumer<String, String> mapPrint = (key, value) -> 
-                                                    System.out.println(key + " is the capital of: "+value);
+                                                    System.out.println(key +
+                                                            " is the capital of: "+value);
         mapCapitalCities.forEach(mapPrint); // Dublin is the capital of: Ireland
-                                            // Washington D.C. is the capital of: USA
+                                            // The Hague is the capital of: Holland
+
+        DoubleConsumer dc = (d) -> System.out.println(d);
+        dc.accept(2.4);// 2.4
+        IntConsumer ic = i -> System.out.println(i);
+        ic.accept(2);// 2
+        LongConsumer lc = (long lg) -> System.out.println(lg);
+        lc.accept(8L);// 8
     }
 
     public void function() {
         // Function<T, R> is a functional interface i.e. one abstract method:
         //      R apply(T t)
         Function<String, Integer> fn2 = s -> s.length();
-        System.out.println("Function: " + fn2.apply("Moscow"));// 6
+        System.out.println("Function: " + fn2.apply("London"));// 6
 
         // BiFunction<T, U, R> is a functional interface i.e. one abstract method:
         //      R apply(T t, U u)
-        BiFunction<String, String, Integer> biFn   = (s1, s2) -> s1.length() + s2.length();
-        System.out.println("BiFunction: " + biFn.apply("William", "Shakespeare"));// 18
+        BiFunction<String, String, Integer> biFn =
+                (s1, s2) -> s1.length() + s2.length();
+        System.out.println("BiFunction: " +
+                biFn.apply("William", "Shakespeare"));// 18
 
-        BiFunction<String, String, String> biFn2   = (s1, s2) -> s1.concat(s2);
-        System.out.println("BiFunction: " + biFn2.apply("William ", "Shakespeare"));// William Shakespeare
+        BiFunction<String, String, String> biFn2 =
+                (s1, s2) -> s1.concat(s2);
+        System.out.println("BiFunction: " +
+                biFn2.apply("William ", "Shakespeare"));// William Shakespeare
+
+        DoubleFunction<String> df = (double dbl) -> "" + Math.pow(dbl, 2);
+        System.out.println(df.apply(2.0)); // 4.0
+        DoubleToIntFunction dtoif = dbl -> (int)Math.round(dbl);
+        System.out.println(dtoif.applyAsInt(4.2)); //4
+        DoubleToLongFunction dtolf = (dbl) -> Math.round(dbl);
+        System.out.println(dtolf.applyAsLong(4.0)); //4
     }
     public void unaryBinaryOperator() {
-        // UnaryOperator<T> extends Function<T, T> is a functional interface i.e. one abstract method:
+        // UnaryOperator<T> extends Function<T, T>
         //      T apply(T t)
         UnaryOperator<String> unaryOp = name -> "My name is "+name;
-        System.out.println("UnaryOperator: " + unaryOp.apply("Sean"));// My name is Sean
+        System.out.println("UnaryOperator: " +
+                unaryOp.apply("Sean"));// My name is Sean
 
-        // BinaryOperator<T> extends BiFunction<T, T, T> is a functional interface i.e. one abstract method:
+        // BinaryOperator<T> extends BiFunction<T, T, T>
         //      T apply(T t1, T t2)
         BinaryOperator<String> binaryOp   = (s1, s2) -> s1.concat(s2);
-        System.out.println("BinaryOperator: " + binaryOp.apply("William ", "Shakespeare"));// William Shakespeare
+        System.out.println("BinaryOperator: " +
+                binaryOp.apply("William ", "Shakespeare"));// William Shakespeare
     }
 }
