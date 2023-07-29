7e62e70391b9469b3262372b8f080b0b4200dd28
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-07-29 17:51:00 +0100

    Add files via upload

---
+++ ch8/animals/Dog.java
@@ -0,0 +1,23 @@
+package animals;
+
+public class Dog {
+    private String dogName;
+    protected int age;
+    public Dog(String dogName){
+        this.dogName = dogName;
+    }
+    public String getDogName(){
+        return dogName;
+    }
+    void pkgPrivate(){}
+}
+class Cat{  // package private
+    Cat(){}
+    public void testDogAccess(){
+        Dog d = new Dog("Rex");
+//        d.dogName = "Abc"; // dogName is private to Dog
+        d.age = 2;
+        d.pkgPrivate(); //ok
+    }
+}
+
