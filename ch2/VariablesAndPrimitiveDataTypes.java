5694eff93a8c992cf5d51ede51bc5530985f9f6d
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-07-29 12:10:34 +0100

    Add files via upload

--- ch2/VariablesAndPrimitiveDataTypes.java
+++ ch2/VariablesAndPrimitiveDataTypes.java
@@ -1,7 +1,26 @@
 package ch2;
 
 public class VariablesAndPrimitiveDataTypes {
+    public static void integerLiterals(){
+        int a = 10;     // decimal
+        int b = 0b1010; // binary
+        int c = 012;    // octal
+        int d = 0xa;    // hexadecimal
+
+        System.out.println(a + ", "+ b + ", "+c+ ", "+d);
+    }
+    public static void screenOutput(){
+        String name = "James";
+        int age = 23;
+        double salary = 50_000.0;
+        // Details: James, 23, 50000.0
+        String output = "Details: "+name+", "+age+", "+salary;
+        System.out.println(output);
+    }
     public static void main(String[] args) {
+        screenOutput();
+//        integerLiterals();
+
 //        int age = 25;
 //        System.out.println(age);
 
@@ -32,36 +51,39 @@ public class VariablesAndPrimitiveDataTypes {
         // binary = 16 + 8 + 4 + 2
         //int bin = 0b11110;
         //System.out.println(bin);
-        byte bin = 0b00010011;   // 19 = 16+2+1
-        bin = 0b01111111;   // +127
-        System.out.println("0b01111111 == "+bin);
-        bin = (byte)0b10000000;   // -128
-        System.out.println("(byte)0b10000000 == "+bin);
-        bin = 0b01;   // +1
-        bin = 0b11;   // +3 (1+2)
-        bin = 0b111;   // +7 (1 + 2 + 4)
-        bin = 0b1111;   // +15 (1 + 2 + 4 + 8)
-        bin = 0b1_1111;   // +31 (1 + 2 + 4 + 8 + 16)
-        bin = 0b11_1111;   // +63 (1 + 2 + 4 + 8 + 16 + 32)
-        bin = 0b111_1111;   // +127 (1 + 2 + 4 + 8 + 16 + 32 + 64)
-        bin = 0b0111_1111;   // +127 (1 + 2 + 4 + 8 + 16 + 32 + 64)
-        bin = (byte)0b1111_1111; // -1 (-128 + 127)
-        bin = (byte)0b10010011; // -109 (-128 + 19)    1001_0011
-        System.out.println("byte == "+bin);
-        int i = 0b10010011; // +147 (+128 + 19)  00000000_00000000_00000000_10010011
-        System.out.println("int == "+i);
-        i = 0b1111_1111; // +255 (+128 + 127)  00000000_00000000_00000000_11111111
-        System.out.println("int == "+i);
-        i = 0b1000_0000; // 128 00000000_00000000_00000000_10000000
-        System.out.println("int == "+i);
-
-        // integral types - have integer values and can be created from
-        // integer literals
-        byte b9=8;
-        char c9=9;
-        int i9=9;
-        short s9=9;
-        long l9=0;
+//        byte b = 0b00010011;   // 19 = 16+2+1
+//        b = 0b0111_1111;   // +127 (1 + 2 + 4 + 8 + 16 + 32 + 64)
+//        System.out.println("0b01111111 == "+b);
+//        b = (byte)0b1000_0000;   // -128
+//        System.out.println("(byte)0b10000000 == "+b);
+//        b = 0b01;   // +1
+//        b = 0b11;   // +3 (1+2)
+//        b = 0b111;   // +7 (1 + 2 + 4)
+//        b = 0b1111;   // +15 (1 + 2 + 4 + 8)
+//        b = 0b1_1111;   // +31 (1 + 2 + 4 + 8 + 16)
+//        b = 0b11_1111;   // +63 (1 + 2 + 4 + 8 + 16 + 32)
+//        b = 0b111_1111;   // +127 (1 + 2 + 4 + 8 + 16 + 32 + 64)
+////        System.out.println("byte == "+b);
+//        b = (byte)0b1111_1111; // -1 (-128 + 127)
+//        b = (byte)0b10010011; // -109 (-128 + 19)    1001_0011
+//        System.out.println("(byte)0b10010011 == "+b);
+//        int i = 0b10010011; // +147 (+128 + 19)  00000000_00000000_00000000_10010011
+//        System.out.println("i == "+i);
+//        i = 0b1111_1111; // +255 (+128 + 127)  00000000_00000000_00000000_11111111
+//        System.out.println("i == "+i);
+//        i = 0b1000_0000; // 128 00000000_00000000_00000000_10000000
+//        System.out.println("i == "+i);
+//
+//        i = 0b10000000_00000000_00000000_00000000; // 0b pattern is viewed an an int, so no cast needed
+//        System.out.println("i == "+i);
+//        System.out.println("Integer.MIN_VALUE == "+Integer.MIN_VALUE);
+//        // integral types - have integer values and can be created from
+//        // integer literals
+//        byte b9=8;
+//        char c9=9;
+//        int i9=9;
+//        short s9=9;
+//        long l9=0;
 //
 //        int x  = 10;
 //        long n = 10L;
@@ -73,6 +95,10 @@ public class VariablesAndPrimitiveDataTypes {
 //
 //        boolean b1 = true;
 //        boolean b2 = false;
+//        float d1 = 1.3e+5f;
+//        System.out.println(d1); // 130000.0
+//        float d2 = 1.3e-1f;
+//        System.out.println(d2); // 0.13
 
     }
 }
