2d747196070aae4dfd25d46a7109b74b593353b8
Sean-D-Kennedy <109792056+Sean-D-Kennedy@users.noreply.github.com>
2023-11-05 14:58:13 +0000

    Add files via upload

--- ch8/BasicEncapsulation.java
+++ ch8/BasicEncapsulation.java
@@ -5,6 +5,49 @@ class Adult{
     private int age;
 
     Adult(String name, int age) {
+        setAge(age);
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if(isAgeOk(age)){
+            this.age = age;
+        }else{
+            this.age = -1; // error state
+        }
+    }
+    private boolean isAgeOk(int age){ // private
+        return age >= 18 ? true :false ; // ternary operator
+    }
+}
+
+ public class BasicEncapsulation {
+    public static void main(String[] args) {
+        Adult john = new Adult("John", 20);
+        System.out.println(john.getName() + " "
+                + john.getAge());   // John 20
+        //john.age = -99; // 'age' is private
+        john.setAge(-99); // uh-oh!
+        System.out.println(john.getName() + " "
+                + john.getAge());   // John -99
+    }
+}
+/*
+package ch8;
+
+class Adult{
+    private String name;
+    private int age;
+
+    Adult(String name, int age) {
         if(isAgeOk(age)){
             this.age = age;
             this.name = name;
@@ -48,6 +91,8 @@ class Adult{
                 + john.getAge());   // John -99
     }
 }
+
+ */
 /*
 class Adult{
     private String name;
